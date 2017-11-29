1a7443a315c3d138e5bfd3e063a5c96e4f58a6bd
den <denniskitun@gmail.com>
2017-11-29 18:27:04 +0200

    changed delay on apply

--- n_a_p/n_a_p/pipelines.py
+++ n_a_p/n_a_p/pipelines.py
@@ -8,9 +8,9 @@ class NAPPipeline(object):
 
     def process_item(self, item, spider):
         if type(item) is items.ProductItem:
-            tasks.add_product.delay([dict(item),])
+            tasks.add_product.apply([dict(item),])
             # tasks.add_product.delay([dict(item),])
         if type(item) is items.PriceItem:
-            tasks.add_price.delay([dict(item),])
+            tasks.add_price.apply([dict(item),])
             # tasks.add_price.delay([dict(item),])
         return item
